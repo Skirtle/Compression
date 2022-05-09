ea8ff7a52e77c0ab10c93308bbf9c27392c04259
Dalton <daltonkajander@knights.ucf.edu>
2022-05-08 22:13:24 -0400

    Update test.cpp

--- Compression/Compression/test.cpp
+++ Compression/Compression/test.cpp
@@ -1,7 +1,24 @@
+// Libraries
 #include <iostream>
 
+
+// Defines
+#define SLIDE_WINDOW 15
+#define LOOK_AHEAD_BUFFER 4
+#define SEARCH_BUFFER 11
+
+// Prototypes
+char LZSS(std::string input);
+
+
 int main() {
-	std::cout << "Hello, world!\n";
+	std::string helloStr = "repetitive repeat";
+	std::cout << helloStr;
 
 	return 0;
+}
+
+char LZSS(std::string input) {
+
+	return '0';
 }
